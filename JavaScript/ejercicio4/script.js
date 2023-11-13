4b86ccf20a605d2e9073664717050e8958c66fa8
Gabriel Karajallo <gabrielkarajallo97@iesamachado.org>
2023-11-13 08:53:24 +0100

    JavaScript

---
+++ JavaScript/ejercicio4/script.js
@@ -0,0 +1,26 @@
+function calcularIMC() {
+    // Obtener los valores de altura y masa ingresados por el usuario
+    var alturaCm = parseFloat(document.getElementById("altura").value);
+    var masaKg = parseFloat(document.getElementById("masa").value);
+
+  
+    var alturaM = alturaCm / 100; // Convertir altura a metros
+    var imc = masaKg / (alturaM * alturaM);
+
+    // Mostrar el resultado del IMC
+    var resultado = "Tu IMC es " + imc.toFixed(2) + ". ";
+    
+
+    if (imc < 18.5) {
+        resultado += "Peso inferior al normal.";
+    } else if (imc < 24.9) {
+        resultado += "Normal.";
+    } else if (imc < 29.9) {
+        resultado += "Peso superiro al normal.";
+    } else {
+        resultado += "Obesidad.";
+    }
+
+    // Mostrar el resultado en la página
+    document.getElementById("resultado").innerHTML = resultado;
+}
