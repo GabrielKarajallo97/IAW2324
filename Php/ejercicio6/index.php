e8086019794d65c4bec46f819d78559fca9239b5
Gabriel Karajallo <gabrielkarajallo97@iesamachado.org>
2023-11-23 10:33:12 +0100

    php

---
+++ Php/ejercicio6/index.php
@@ -0,0 +1,15 @@
+<?php
+    //Comprobar si el tipo de una variable es entero:
+    $numero1 = 150;
+    var_dump($numero1);
+
+    $numero2 =1.50;
+    var_dump($numero2);
+
+    //Comprobar si el tipo de la variabe es float
+    $x = 10.23;
+    var_dump(is_float($x));
+
+
+
+?>
