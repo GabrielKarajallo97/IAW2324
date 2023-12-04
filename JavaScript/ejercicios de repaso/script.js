d8d31c80db4b406fb92c17ec6832e61c1b547e21
GabrielKarajallo97 <gabrielkarajallo97@iesamachado.org>
2023-12-04 17:36:17 +0100

    js ej

---
+++ JavaScript/ejercicios de repaso/script.js
@@ -0,0 +1,3 @@
+//Crea una función llamada procesarArray que toma un array y una función de transformación como argumentos 
+//y devuelve un nuevo array que es el resultado de aplicar la función de transformación a cada elemento del array original.
+
