84b45cd03fb68717aede477dbc86956e9f92c3db
GabrielKarajallo97 <gabrielkarajallo97@iesamachado.org>
2023-12-05 16:40:36 +0100

    cambios

--- JavaScript/ejercicios de repaso/script.js
+++
@@ -1,8 +0,0 @@
-        //ejercicio1
-        let total;
-
-        function calcularPrecio(precio){
-            return (precio * 0,21)+ precio;
-        }
-        
-        console.log(calcularPrecio(50));
