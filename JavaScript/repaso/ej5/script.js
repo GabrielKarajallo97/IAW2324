51312021058ca3246a49aef1bace483127f3b071
GabrielKarajallo97 <gabrielkarajallo97@iesamachado.org>
2023-12-06 18:01:41 +0100

    js

---
+++ JavaScript/repaso/ej5/script.js
@@ -0,0 +1,14 @@
+function redSocial(){
+    const valorInput = document.getElementById('idInput').value;
+    let mensaje = [valorInput];
+    
+
+    const mensajeEnviado = document.getElementById('spanId');
+    mensajeEnviado.textContent = mensaje;
+
+   
+    
+    
+    
+    
+}
