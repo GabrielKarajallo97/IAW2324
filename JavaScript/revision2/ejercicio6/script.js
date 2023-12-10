49cbf98c8fcca83b2403e03b405e3f571a58ccd2
GabrielKarajallo97 <gabrielkarajallo97@iesamachado.org>
2023-12-10 22:05:07 +0100

    js

--- JavaScript/revision2/ejercicio6/script.js
+++ JavaScript/revision2/ejercicio6/script.js
@@ -1,1 +1,37 @@
+function validarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var email = document.getElementById('email').value;
+    var dni = document.getElementById('dni').value;
+    var pin = document.getElementById('pin').value;
+    var pinRepetido = document.getElementById('pinRepetido').value;
 
+  
+    if (!nombre || !apellido || !email || !dni || !pin || !pinRepetido) {
+        document.getElementById('errorMensaje').innerText = 'Todos los campos marcados con * son requeridos.';
+        return false;
+    }
+
+  
+         {
+        document.getElementById('errorMensaje').innerText = 'El DNI debe contener solo números.';
+        return false;
+    }
+
+ 
+    if (pin !== pinRepetido) {
+        document.getElementById('errorMensaje').innerText = 'Los PINS no coinciden.';
+        return false;
+    }
+
+    // Restablecer mensaje de error si todo está bien
+    document.getElementById('errorMensaje').innerText = '';
+
+    // Crear nombre de usuario
+    var nombreUsuario = nombre.substring(0, 2) + apellido.substring(0, 2) + telefono.slice(-3);
+
+    // Mostrar mensaje de éxito
+    alert('Usuario creado con éxito. Nombre de usuario: ' + nombreUsuario);
+
+    return false; 
+}
