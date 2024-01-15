20ef07047ffd0d3e563236fb4e31fb757f44b0f7
Gabriel Karajallo <gabrielkarajallo97@iesamachado.org>
2024-01-15 12:26:17 +0100

    php

--- php/mysql/gestion/includes/home.php
+++ php/mysql/gestion/includes/home.php
@@ -1,98 +1,189 @@
 <!-- Header -->
-<?php include "../header.php"?>
+<?php include "../header.php" ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Incidencias</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <!-- <a class="navbar-brand" href="#">Incidencias</a> -->
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Añadir incidencia</a>
+          <a href="create.php" class='btn btn-outline-dark mb-2'> <i class="bi bi-person-plus"></i> Inicio </a>
+        </li>
+        <li class="nav-item">
+          <a href="create.php" class='btn btn-outline-dark mb-2'> <i class="bi bi-person-plus"></i> Añadir
+            incidencia</a>
+        </li>
+        <li class="nav-item">
+          <a href="create.php" class='btn btn-outline-dark mb-2'> <i class="bi bi-person-plus"></i> Administración</a>
         </li>
       </ul>
-      <span class="navbar-text">
-        <?php include "index.php";
-          echo '$usuario = htmlspecialchars($_POST["usuario"]);'
-        ?>
-      </span>
     </div>
   </div>
 </nav>
-  <div class="container">
-    <h1 class="text-center" >Gestión de incidencias (CRUD)</h1>
-      <a href="create.php" class='btn btn-outline-dark mb-2'> <i class="bi bi-person-plus"></i> Añadir incidencia</a>
-        <table class="table table-striped table-bordered table-hover">
-          <thead class="table-dark">
-            <tr>
-              <th  scope="col">ID</th>
-              <th  scope="col">Planta</th>
-              <th  scope="col">Aula</th>
-              <th  scope="col">Descripción</th>
-              <th  scope="col">Fecha alta</th>
-              <th  scope="col">Fecha revisión</th>
-              <th  scope="col">Fecha solución</th>
-              <th  scope="col">Comentario</th>
-              <th  scope="col" colspan="3" class="text-center">Operaciones</th>
-            </tr>  
-          </thead>
-            <tbody>
-              <tr>
- 
-          <?php
-            $query="SELECT * FROM incidencia";               
-            $vista_incidencias= mysqli_query($conn,$query);
+<h1 class="text-center">Bienvenido, (usuario)</h1>
 
-            while($row= mysqli_fetch_assoc($vista_incidencias)){
-              $id = $row['id'];                
-              $planta = $row['planta'];        
-              $aula = $row['aula'];         
-              $descripcion = $row['descripcion'];        
-              $fecha_alta = $row['fecha_alta'];        
-              $fecha_revision = $row['fecha_revision'];        
-              $fecha_resolucion = $row['fecha_resolucion'];        
-              $comentario = $row['comentario']; 
-              echo "<tr >";
-              echo " <th scope='row' >{$id}</th>";
-              echo " <td > {$planta}</td>";
-              echo " <td > {$aula}</td>";
-              echo " <td >{$descripcion} </td>";
-              echo " <td >{$fecha_alta} </td>";
-              echo " <td >{$fecha_revision} </td>";
-              echo " <td >{$fecha_resolucion} </td>";
-              echo " <td >{$comentario} </td>";
-              echo " <td class='text-center'> <a href='view.php?incidencia_id={$id}' class='btn btn-primary'> <i class='bi bi-eye'></i> Ver</a> </td>";
-              echo " <td class='text-center' > <a href='update.php?editar&incidencia_id={$id}' class='btn btn-secondary'><i class='bi bi-pencil'></i> Editar</a> </td>";
-              echo " <td class='text-center'>  <a href='delete.php?eliminar={$id}' class='btn btn-danger'> <i class='bi bi-trash'></i> Eliminar</a> </td>";
-              echo " </tr> ";
-                  }  
-                ?>
-              </tr>  
-            </tbody>
-        </table>
-  </div>
-<div class="container text-center mt-5">
-      <a href="../index.php" class="btn btn-warning mt-5"> Volver </a>
-    <div>
+
+<div class="container">
+  <table class="table table-striped table-bordered table-hover">
+    <thead class="nombre-tabla">
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Planta</th>
+        <th scope="col">Aula</th>
+        <th scope="col">Descripción</th>
+        <th scope="col">Fecha alta</th>
+        <th scope="col">Fecha revisión</th>
+        <th scope="col">Fecha solución</th>
+        <th scope="col">Comentario</th>
+        <th scope="col" colspan="3" class="text-center">Operaciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+
+        <?php
+        $query = "SELECT * FROM incidencia";
+        $vista_incidencias = mysqli_query($conn, $query);
+
+        while ($row = mysqli_fetch_assoc($vista_incidencias)) {
+          $id = $row['id'];
+          $planta = $row['planta'];
+          $aula = $row['aula'];
+          $descripcion = $row['descripcion'];
+          $fecha_alta = $row['fecha_alta'];
+          $fecha_revision = $row['fecha_revision'];
+          $fecha_resolucion = $row['fecha_resolucion'];
+          $comentario = $row['comentario'];
+          echo "<tr >";
+          echo " <th scope='row' >{$id}</th>";
+          echo " <td > {$planta}</td>";
+          echo " <td > {$aula}</td>";
+          echo " <td >{$descripcion} </td>";
+          echo " <td >{$fecha_alta} </td>";
+          echo " <td >{$fecha_revision} </td>";
+          echo " <td >{$fecha_resolucion} </td>";
+          echo " <td >{$comentario} </td>";
+          echo " <td class='text-center'> <a href='view.php?incidencia_id={$id}' class='btn btn-primary'> <i class='bi bi-eye'></i></a> </td>";
+          echo " <td class='text-center' > <a href='update.php?editar&incidencia_id={$id}' class='btn btn-secondary'><i class='bi bi-pencil'></i></a> </td>";
+          echo " <td class='text-center'>  <a href='delete.php?eliminar={$id}' class='btn btn-danger'> <i class='bi bi-trash'></i></a> </td>";
+          echo " </tr> ";
+        }
+        ?>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<div class=" text-center mt-5">
+  <a href="../index.php" class="btn btn-warning mt-5"> Volver </a>
+  <div>
 
 
 
     <style>
+      nav {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+
       body {
-    display: block;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: 0;
-    /* background-color: #534859; */
-    background: #092756;
-    background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -moz-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -moz-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-    background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -webkit-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -webkit-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-    background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -o-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -o-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-    background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -ms-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -ms-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-    background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), linear-gradient(to bottom, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), linear-gradient(135deg, #670d10 0%, #092756 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3E1D6D', endColorstr='#092756', GradientType=1)
-  }
+        display: block;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        /* background-color: #534859; */
+        background: #092756;
+        background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -moz-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -moz-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -webkit-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -webkit-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+        background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -o-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -o-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+        background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -ms-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -ms-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), linear-gradient(to bottom, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), linear-gradient(135deg, #670d10 0%, #092756 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3E1D6D', endColorstr='#092756', GradientType=1)
+      }
+
+      /*-----------Contenedor--------------*/
+      .container {
+        background-color: #f3f4f5;
+        border-radius: 8px;
+        padding: 0px;
+        border: 0px;
+        box-shadow: 20px 30px 50px rgba(1, 1, 1, 0.5);
+      }
+
+      /*-------Titulo---------------*/
+      h1 {
+        font-family: 'Paytone One', sans-serif;
+        color: #fff;
+        text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        letter-spacing: 1px;
+        text-align: center;
+        margin-bottom: 20px;
+        margin-top: 80px
+      }
+
+      /*----------NAV--------------------*/
+      .navbar{
+    position: absolute;
+    top: 5%;
+}
+.nav-item a{
+  color:  #fff;
+  border: 0px;
+  font-size: 1.2rem;
+  padding: 10px;
+  margin: 10px;
+}
+
+      /*------Tabla--------*/
+      .nombre-tabla {
+        border-style: none;
+        vertical-align: middle;
+        background-color: #b2bfcf;
+        border-top-left-radius: 8px;
+      }
+
+      .table>thead {
+        vertical-align: middle;
+      }
+
+      tbody,
+      td,
+      tfoot,
+      th,
+      thead,
+      tr {
+        border-color: inherit;
+        border-style: none;
+        border-width: 0;
+        padding: 10px;
+      }
+
+
+      /*------------------Boton VOLVER------------------- */
+      .mt-5 a {
+        /* margin-top: 3rem!important; */
+        border: 0px;
+        background-color: #b2bfcf;
+        box-shadow: 20px 30px 50px rgba(1, 1, 1, 0.5);
+      }
+
+      /*------------OPERACIONES----------------*/
+      .btn-primary {
+        color: #fff;
+        background-color: #b2bfcf;
+        border: 0px;
+      }
+
+      .btn-secondary {
+        color: #fff;
+        background-color: #b2bfcf;
+        border: 0px;
+      }
+
+      .btn-danger {
+        color: #fff;
+        background-color: #b2bfcf;
+        border: 0px;
+      }
     </style>
-<?php include "../footer.php" ?>
+    <?php include "../footer.php" ?>
