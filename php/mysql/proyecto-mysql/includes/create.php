331c53402da83a30e29beab64d5a5b792d219bb1
Gabriel Karajallo <gabrielkarajallo97@iesamachado.org>
2024-01-22 14:20:26 +0100

    php

--- php/mysql/proyecto-mysql/includes/create.php
+++ php/mysql/proyecto-mysql/includes/create.php
@@ -9,7 +9,7 @@ if (isset($_POST['crear'])) {
   $fecha_alta = htmlspecialchars($_POST['fecha_alta']);
   $fecha_revision = htmlspecialchars($_POST['fecha_revision']);
   $fecha_resolucion = htmlspecialchars($_POST['fecha_resolucion']);
-
+ 
   $query = "INSERT INTO incidencia(planta, aula, descripcion, fecha_alta, fecha_revision, fecha_resolucion, comentario) VALUES('{$planta}','{$aula}','{$descripcion}','{$fecha_alta}','{$fecha_revision}','{$fecha_resolucion}','{$comentario}')";
   $resultado = mysqli_query($conn, $query);
 
@@ -19,9 +19,9 @@ if (isset($_POST['crear'])) {
     echo "<script type='text/javascript'>alert('¡Incidencia añadida con éxito!')</script>";
   }
 }
-if($_SESSION['user']){
+if ($_SESSION['user']) {
 
-} else{
+} else {
   header("location: ../index.php");
 }
 ?>
@@ -50,11 +50,21 @@ if($_SESSION['user']){
   <form action="" method="post">
     <div class="form-group">
       <label for="planta" class="form-label">Planta</label>
-      <input type="text" name="planta" class="form-control">
+      <select name="planta" class="form-select" aria-label="Default select example">
+        <option selected>Seleccione Planta</option>
+        <option value="1">Baja</option>
+        <option value="2">Primera</option>
+        <option value="3">Segunda</option>
+      </select>
     </div>
     <div class="form-group">
       <label for="aula" class="form-label">Aula</label>
-      <input type="text" name="aula" class="form-control">
+      <select name="aula" class="form-select" aria-label="Default select example">
+        <option selected>Seleccione Aula</option>
+        <option value="1">100</option>
+        <option value="2">101</option>
+        <option value="3">102</option>
+      </select>
     </div>
     <div class="form-group">
       <label for="descripcion" class="form-label">Descripcion</label>
@@ -83,83 +93,85 @@ if($_SESSION['user']){
 </div>
 
 
-    <style>
-      body {
-        display: block;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-        background: #092756;
-        background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -moz-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -moz-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -webkit-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -webkit-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-        background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -o-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -o-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-        background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -ms-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -ms-linear-gradient(-45deg, #670d10 0%, #092756 100%);
-        background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), linear-gradient(to bottom, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), linear-gradient(135deg, #670d10 0%, #092756 100%);
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3E1D6D', endColorstr='#092756', GradientType=1)
-      }
+<style>
+  body {
+    display: block;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background: #092756;
+    background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -moz-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -moz-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+    background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -webkit-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -webkit-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+    background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -o-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -o-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+    background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), -ms-linear-gradient(top, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), -ms-linear-gradient(-45deg, #670d10 0%, #092756 100%);
+    background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104, 128, 138, .4) 10%, rgba(138, 114, 76, 0) 40%), linear-gradient(to bottom, rgba(57, 173, 219, .25) 0%, rgba(42, 60, 87, .4) 100%), linear-gradient(135deg, #670d10 0%, #092756 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#3E1D6D', endColorstr='#092756', GradientType=1)
+  }
 
-      /*----------NAV--------------------*/
-      nav {
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-      .navbar {
-        position: absolute;
-        top: 5%;
-      }
+  /*----------NAV--------------------*/
+  nav {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 
-      .nav-item a {
-        color: #fff;
-        border: 0px;
-        margin-right: 10px;
-      }
+  .navbar {
+    position: absolute;
+    top: 5%;
+  }
 
-      /*-----------Contenedor--------------*/
-      .container {
-        max-width: 400px;
-        background-color: #f3f4f5;
-        border-radius: 8px;
-        padding: 0px;
-        border: 0px;
-        box-shadow: 20px 30px 50px rgba(1, 1, 1, 0.5);
-        position: absolute;
-        top: 47%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
+  .nav-item a {
+    color: #fff;
+    border: 0px;
+    margin-right: 10px;
+  }
 
-      .container2 {
-        position: absolute;
-        top: 71%;
-        left: 51%;
-        transform: translate(-50%, -50%);
-        height: 5px;
-      }
+  /*-----------Contenedor--------------*/
+  .container {
+    max-width: 400px;
+    background-color: #f3f4f5;
+    border-radius: 8px;
+    padding: 0px;
+    border: 0px;
+    box-shadow: 20px 30px 50px rgba(1, 1, 1, 0.5);
+    position: absolute;
+    top: 47%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .container2 {
+    position: absolute;
+    top: 71%;
+    left: 51%;
+    transform: translate(-50%, -50%);
+    height: 5px;
+  }
 
 
-      form {
-        padding: 20px;
-      }
+  form {
+    padding: 20px;
+  }
 
-      /*-------Titulo---------------*/
-      h1 {
-        font-family: 'Paytone One', sans-serif;
-        color: #fff;
-        text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-        letter-spacing: 1px;
-        text-align: center;
-        margin-bottom: 20px;
-        margin-top: 80px
-      }
-      
-      /*------BOTON------*/
-      .boton{
-        text-align: center;
-      }
-      .boton button{
-        background-color: #b2bfcf;
-        border: 0px;
-      }
-    </style>
+  /*-------Titulo---------------*/
+  h1 {
+    font-family: 'Paytone One', sans-serif;
+    color: #fff;
+    text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    letter-spacing: 1px;
+    text-align: center;
+    margin-bottom: 20px;
+    margin-top: 80px
+  }
+
+  /*------BOTON------*/
+  .boton {
+    text-align: center;
+  }
+
+  .boton button {
+    background-color: #b2bfcf;
+    border: 0px;
+  }
+</style>
