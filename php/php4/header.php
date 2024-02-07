d8c9b139d5620a6ff94c766325ec95bed25bb1be
GabrielKarajallo97 <gabrielkarajallo97@iesamachado.org>
2024-02-07 14:55:37 +0100

    php

--- php/php4/header.php
+++ php/php4/header.php
@@ -1,5 +1,1 @@
-<?php
-// Utilizar la función header para redirigir a otra página
-header("Location: /php/php3/saludo.php");
-exit; // Asegurar que el script se detenga después de enviar la cabecera de redirección
-?>
+<?php header("Location: /php/php3/saludo.php") ?>;
